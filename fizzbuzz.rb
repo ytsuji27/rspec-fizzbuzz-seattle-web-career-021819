5cadca62a2ac20afd5d5297684c8aaf6ab56d1ed
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-07 01:05:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,9 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   if (num % 3 == 0) && (num % 5 == 0)
-    
-  
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
 end

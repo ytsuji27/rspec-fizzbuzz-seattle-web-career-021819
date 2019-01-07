744638eafe957a7ff53036d3df61921e3bc5c9c5
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-07 01:04:07 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
+def fizzbuzz(num)
+  if (num % 3 == 0) && (num % 5 == 0)
+    
   
 end
